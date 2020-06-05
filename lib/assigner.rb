9bce8446c475a22ba1455374e0160c7a5a62a7ce
Andy Wendt <awendt@mavenlink.com>
2020-06-05 08:59:58 -0600

    Add mocked out assigner class

--- lib/assigner.rb
+++ lib/assigner.rb
@@ -1,2 +1,22 @@
 class Assigner
+  def initialize(guides, participants)
+    @guides = guides
+    @participants = participants
+  end
+
+  def teams
+    [
+        Team.new('guide a', ['person a', 'person b']),
+        Team.new('guide b', ['person c'])
+    ]
+  end
+
+  class Team
+    def initialize(guide, participants)
+      @guide = guide
+      @participants = participants
+    end
+
+    attr_reader :guide, :participants
+  end
 end
--- spec/assigner_spec.rb
+++ spec/assigner_spec.rb
@@ -2,9 +2,18 @@ require 'rspec'
 require 'assigner'
 
 describe Assigner do
-  context 'when condition' do
-    it 'succeeds' do
-      expect(true).to be(true)
-    end
+  it 'sorts guides and participants into teams so that they are equal' do
+    participants = ['person a', 'person b', 'person c']
+    guides = ['guide a', 'guide b']
+
+    result = Assigner.new(guides, participants).teams
+
+    team1 = result[0]
+    expect(team1.guide).to eq(guides[0])
+    expect(team1.participants).to eq([participants[0], participants[1]])
+
+    team2 = result[1]
+    expect(team2.guide).to eq(guides[1])
+    expect(team2.participants).to eq([participants[2]])
   end
 end
