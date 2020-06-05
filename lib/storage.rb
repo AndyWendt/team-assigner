f850a4d22d78ce504de68fb3a2ed40d86404817e
Andy Wendt <awendt@mavenlink.com>
2020-06-05 16:24:11 -0600

    Remove static interfacee

--- lib/storage.rb
+++ lib/storage.rb
@@ -9,42 +9,22 @@ class Storage
     File.file?(path(file))
   end
 
-  def self.exists?(file)
-    self.new.exists?(file)
-  end
-
   def storage_directory
     directory
-  end
-
-  def self.storage_directory
-    self.new.storage_directory
   end
 
   def path(file)
     "#{directory}/#{file}"
   end
 
-  def self.path(file)
-    self.new.path(file)
-  end
-
   def delete(file)
     File.delete(path(file)) if exists?(file)
-  end
-
-  def self.delete(file)
-    self.new.delete(file)
   end
 
   def write_csv(out_file, lines)
     CSV.open(path(out_file), "w") do |csv|
       lines.each { |line| csv << line }
     end
-  end
-
-  def self.write_csv(out_file, lines)
-    self.new.write_csv(out_file, lines)
   end
 
   private
