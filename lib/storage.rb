3ccae40e9f47112c94d95760a15879a6449d1e9a
Andy Wendt <awendt@mavenlink.com>
2020-06-05 15:56:19 -0600

    Implement assigner writing out a csv

--- lib/storage.rb
+++ lib/storage.rb
@@ -1,12 +1,26 @@
+require 'csv'
+
 class Storage
   def self.exists?(file)
-    File.file?("#{storage_directory}/#{file}")
+    File.file?(path(file))
   end
-
-  private
 
   def self.storage_directory
     current_directory = File.expand_path(File.dirname(__FILE__))
     "#{current_directory}/../storage"
+  end
+
+  def self.path(file)
+    "#{storage_directory}/#{file}"
+  end
+
+  def self.delete(file)
+    File.delete(path(file)) if exists?(file)
+  end
+
+  def self.write_csv(out_file, lines)
+    CSV.open(path(out_file), "w") do |csv|
+      lines.each { |line| csv << line }
+    end
   end
 end
