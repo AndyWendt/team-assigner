a0b3bdee2d094810e1770917113cc83a74b42ed5
Andy Wendt <awendt@mavenlink.com>
2020-06-05 18:31:41 -0600

    Check storage

--- spec/storage_spec.rb
+++ spec/storage_spec.rb
@@ -3,7 +3,15 @@ require 'storage'
 
 describe Storage do
   it 'checks if a file exists' do
-    expect(Storage.new.exists?('.gitignore')).to be_truthy
-    expect(Storage.new.exists?('nothing.txt')).to be_falsey
+    expect(described_class.new.exists?('.gitignore')).to be_truthy
+    expect(described_class.new.exists?('nothing.txt')).to be_falsey
+  end
+
+  it 'overwrites a file' do
+    file = 'file.test'
+    instance = described_class.new('spec/storage')
+    instance.write_csv(file, [['one']])
+    instance.write_csv(file, [['two']])
+    expect(instance.read(file)[0][0]).to eq('two')
   end
 end
